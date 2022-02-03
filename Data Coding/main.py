c6807d5f5b1ed65ae3fd73b027f91e4a1e1cee64
geekblack22 <geekblack22@users.noreply.github.com>
2022-02-03 14:27:46 -0500

    Update main.py

--- Data Coding/main.py
+++ Data Coding/main.py
@@ -9,11 +9,10 @@ expresion = pd.DataFrame(problem, columns=['expression']).to_numpy()
 
 #start_equation = "5.255+32+49*15+32+9-5+9+9"
 #store the equations from the excel sheet as strings
-start_equation = str(start[1]).lstrip('[').rstrip(']')
-start_equation_list = list(start_equation)
-
-equation = str(expresion[1]).lstrip('[').rstrip(']')
-equation_list = list(equation)
+ 
+ #start_equation_list = list(start_equation)
+#equation = str(expresion[1]).lstrip('[').rstrip(']')
+# equation_list = list(equation)
 
 goal_eq = str(goal[27]).lstrip('[').rstrip(']')
 goal_list = list(goal_eq)
@@ -23,8 +22,14 @@ def add_to_before(arr,index,type):
     for i in range(index-1,-1,-1):
         #break out of the loop if another operation symbol is reached
         if (not arr[i].isnumeric() and arr[i] != '.'):
-           if(type == "plus" or type == "minus"):
+           if(type == "plus"):
                 if(arr[i] == '*' or arr[i] == '/'):
+                    before = ""
+                    break
+                else:
+                    break
+           if( type == "minus"):
+                if(arr[i] == '*' or arr[i] == '/' or arr[i] == '-'):
                     before = ""
                     break
                 else:
@@ -112,7 +117,12 @@ def removeDuplicates(str,arr):
 
 
 
-
+def remove_non_numeric(arr):
+   
+     for i in range(len(arr)):
+         if(not arr[i].isnumeric()):
+             arr.pop(i)
+     return arr
 #store numbers in their respective operations list
 def store_operation_list(start_equation):
     multi = []
@@ -153,6 +163,8 @@ def store_operation_list(start_equation):
              
               if(add_to_after(start_equation,i,"minus") != ""):
                    sub.append(add_to_after(start_equation,i,"minus"))
+              if(add_to_before(start_equation,i,"minus") != ""):
+                   add.append(add_to_before(start_equation,i,"minus"))
             # if(i == len(start_equation) - 2):
             #     if(start_equation[i-2].isnumeric()):
             #         add.append(start_equation[i-1] + start_equation[i-2])
@@ -171,7 +183,7 @@ def store_operation_list(start_equation):
             #                 add.append(start_equation[i+1] + start_equation[i+2])
             #             else:
             #                 add.append(start_equation[i+1])            
-        if(start_equation_list[i] == '*'):
+        if(start_equation[i] == '*'):
             if(add_to_before(start_equation,i,"multi") != ""):
                    multi.append(add_to_before(start_equation,i,"multi"))
             if(add_to_after(start_equation,i,"multi") != ""):
@@ -195,7 +207,7 @@ def store_operation_list(start_equation):
             #                     multi.append(start_equation[i+1] + start_equation[i+2])
             #                 else:
             #                     multi.append(start_equation[i+1])
-        if(start_equation_list[i] == '/'):
+        if(start_equation[i] == '/'):
             if(add_to_before(start_equation,i,"div") != ""):
                    div.append(add_to_before(start_equation,i,"div"))
             if(add_to_after(start_equation,i,"div") != ""):
@@ -219,12 +231,8 @@ def store_operation_list(start_equation):
             #                     div.append(start_equation[i+1] + start_equation[i+2])
             #                 else:
             #                     div.append(start_equation[i+1])    
-    return removeDuplicates(start_equation,add), removeDuplicates(start_equation,sub), removeDuplicates(start_equation,multi),removeDuplicates(start_equation,div)                
-def remove_non_numeric(arr):
-     for i in range(len(arr)):
-         if( not arr[i].isnumeric()):
-             arr.pop(i)
-     return arr
+    return remove_non_numeric(removeDuplicates(start_equation,add)), remove_non_numeric(removeDuplicates(start_equation,sub)), remove_non_numeric(removeDuplicates(start_equation,multi)),remove_non_numeric(removeDuplicates(start_equation,div))               
+
 #remove duplicates from list of all possible operation list
 def remove_duplicates(arr):
     res = []
@@ -254,28 +262,82 @@ def create_multi_list(arr):
                         val = int(val)
                     multi_vals.append(str(val))
         return remove_duplicates(multi_vals)
+#create list of all possible combinations from subtraction
+def create_sub_list(arr,arr2):
+        sub_vals = []
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if(j != i):
+                    
+                    val = float(arr[i])+float(arr[j])
+                    if(val.is_integer()):
+                        val = int(val)
+                    sub_vals.append(str(val))
+        for i in range(len(arr)):
+            for j in range(len(arr2)):
+                val = float(arr2[j])-float(arr[i])
+                if(val.is_integer()):
+                    val = int(val)
+                sub_vals.append(str(val))
+            
+            return remove_duplicates(sub_vals)
+def create_div_list(arr):
+        div_vals = []
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if(j != i):
+                    val = float(arr[i])/float(arr[j])
+                    if(val.is_integer()):
+                        val = int(val)
+                    div_vals.append(str(val))
+     
+        
+            return remove_duplicates(div_vals)
 #check if an operation was performed 
 def operation_is_calc(opp_list, start, express):
         start_count = 0
         express_count = 0
+        different = False
         for i in range(len(opp_list)):
-            
+            if(start.find(opp_list[i])  == -1) and (express.find(opp_list[i])  != -1):
+                different = True
             if(start.find(opp_list[i])  != -1):
                 start_count += 1
             if(express.find(opp_list[i])  != -1):
                 express_count += 1
-        print(start_count)
-        print(express_count)
-        return express_count != 0 and express_count != start_count
+        print(different)
+        return express_count != 0  and (express_count != start_count  or different) and len(express) < len(start)
 
 
 #add = remove_non_numeric(add)
-add, sub, multi, div = store_operation_list(start_equation)
-is_multi = operation_is_calc(create_multi_list(multi), start_equation,equation)
-is_add = operation_is_calc(create_add_list(add), start_equation,equation)
-print(is_add)
-print(create_multi_list(multi))
-print(is_multi)
-f = 32.0
+response = []
+for i in range(len(start)):
+    start_equation = str(start[i]).lstrip('[').rstrip(']')
 
+
+    equation = str(expresion[i]).lstrip('[').rstrip(']')
+    is_multi = False
+    is_add = False
+    is_sub = False
+    is_div = False
+    add, sub, multi, div = store_operation_list(start_equation)
+    if(not (create_multi_list(multi) is None)):
+        is_multi = operation_is_calc(create_multi_list(multi), start_equation,equation)
+    if(not (create_add_list(add) is None)):
+        is_add = operation_is_calc(create_add_list(add), start_equation,equation)
+    if(not (create_sub_list(sub,add) is None)):
+        is_sub =  operation_is_calc(create_sub_list(sub,add), start_equation,equation)
+    if(not (create_div_list(div) is None)):
+        is_div = operation_is_calc(create_div_list(div), start_equation,equation)
+    if(is_div or is_add or is_multi or is_sub):
+        response.append("CALC")
+    else:
+        response.append("N/A")
+start_equation = str(start[32]).lstrip('[').rstrip(']')
+equation = str(expresion[32]).lstrip('[').rstrip(']')
+add, sub, multi, div = store_operation_list(start_equation)   
+#print(create_sub_list(sub,add))
+
+print(response)
+#print(len(response))
 
